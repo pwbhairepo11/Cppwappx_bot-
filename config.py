b17e9f64b0fae76ccfa2dbac3d0afc5a48de0cc7
pwbhairepo11 <pwbhairepo@gmail.com>
2025-05-22 15:02:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "28460032"
+api_hash = "1457c3ba64719a1e442aae67217b67c2"
+bot_token = "AAFYhCt4La8N42GshgLHgy6nCvcdFpQKqcM"
 auth_users = [7991495621]
